b02215797e4ce3a90ee0b475534398c4f5061aa9
0utKast <educaleft@gmail.com>
2019-02-22 00:38:21 +0000

    Añadido script en package.json para transpilar.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
-import http from "http"; // ES6 syntax
+import "@babel/polyfill";
+import http from "http"; 
 const requestHandler = function(req, res) {
   res.writeHead(200, { "Content-Type": "text/plain" });
   res.end("Hello, World!");
 };
 const server = http.createServer(requestHandler);
 server.listen(8080);
-
-
